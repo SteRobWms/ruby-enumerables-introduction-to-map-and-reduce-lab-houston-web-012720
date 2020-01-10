4c93725184515255e5e85cc9ecddb7fc21ec6870
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-10 05:23:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# My Code here....
+def map_to_negativize(data)
+  i = 0
+  neg_data = []
+  while i < data.length do
+    neg_data << data[i] * -1
+    i += 1
+  end
+end
+
+def map_to_no_change(data)
+end
+
+def map_to_double(data)
+end
+
+def map_to_square(data)
+end
+
+def reduce_to_total(data)
+end
+
+def reduce_to_all_true(data)
+end
+
+def reduce_to_any_true(data)
+end
